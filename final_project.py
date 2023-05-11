28615e13ed21672306b62d6976883bc57cdc8d04
Marie Shotts <mshotts2026@kehillahstudent.org>
2023-05-11 11:20:02 -0700

    change to add comments to existing code

--- final_project.py
+++ final_project.py
@@ -4,25 +4,26 @@ df = pd.read_csv(r'Table (3).csv', sep=',')
 df=df[['GeoFips','GeoName','2021']]
 
 import plotly.express as px
+#sets GeoFips to a string
 df = df.astype({'GeoFips' : 'str'})
-print (df.info())
+#dictionary to make states (aka GeoFips) in abbreviated formate
 fips_abbr = {"53000": 'WA', "10000": 'DE', "11000": 'DC', "55000": 'WI', "54000": 'WV', "15000": 'HI', "12000": 'FL', "56000": 'WY', "72000": 'PR', "34000": 'NJ', "35000": 'NM', "48000": 'TX', "22000": 'LA', "37000": 'NC', "38000": 'ND', "31000": 'NE', "47000": 'TN', "36000": 'NY', "42000": 'PA', "2000": 'AK', "32000": 'NV', "33000": 'NH', "51000": 'VA', "8000": 'CO', "6000": 'CA', "1000": 'AL', "5000": 'AR', "50000": 'VT', "17000": 'IL', "13000": 'GA', "18000": 'IN', "19000": 'IA', "25000": 'MA', "4000": 'AZ', "16000": 'ID', "9000": 'CT', "23000": 'ME', "24000": 'MD', "40000": 'OK', "39000": 'OH', "49000": 'UT', "29000": 'MO', "27000": 'MN', "26000": 'MI', "44000": 'RI', "20000": 'KS', "30000": 'MT', "28000": 'MS', "45000": 'SC', "21000": 'KY', "41000": 'OR', "46000": 'SD'}
 df["state_abbr"] = df["GeoFips"].map (fips_abbr)
-print (df)
+#sets up cloropleth map
 fig = px.choropleth(df,
                     locations='state_abbr', 
                     locationmode="USA-states", 
                     scope="usa",
                     color='2021',
                     color_continuous_scale="Purples", 
-                    #need to figure out how to set scale from 0 to 120,000,000
                     )
-#colorbar ticks
+#changes words at top of colorbar to "Gross Inflow of Earnings"
 fig.layout.coloraxis.colorbar.title.text = "Gross Inflow of Earnings"
+#changes colorbar ticks to 0 to 120 M
 fig.layout.coloraxis.colorbar.tickvals = [0, 120000000]
 fig.layout.coloraxis.cmax = 120000000
 fig.layout.coloraxis.cauto = False
-
+#adds title at top of webpage
 fig.update_layout(
       title_text = '2021 Inflow of Earnings by State',
       title_font_family="Times New Roman",
@@ -30,4 +31,5 @@ fig.update_layout(
       title_font_color="black", 
       title_x=0.45, 
          )
+#shows figure
 fig.show()
